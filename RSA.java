7dda4a978d22834fbc32bfd178b4180e6f3f28e7
Osman-Osmanajj <153840822+Osman-Osmanajj@users.noreply.github.com>
2024-05-22 22:26:35 +0200

    RSA File

--- RSA.java
+++ RSA.java
@@ -1,2 +1,40 @@
+import javax.crypto.Cipher;
+import java.security.*;
+import java.util.Base64;
+
 public class RSA {
+    private KeyPair keyPair;
+
+    public RSA() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        keyPair = keyPairGenerator.generateKeyPair();
+    }
+
+    public PublicKey getPublicKey() {
+        return keyPair.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return keyPair.getPrivate();
+    }
+
+    public byte[] encrypt(byte[] data, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(data);
+    }
+
+    public byte[] decrypt(byte[] data, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(data);
+    }
+    public static String toBase64(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    public static byte[] fromBase64(String data) {
+        return Base64.getDecoder().decode(data);
+    }
 }
